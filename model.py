c4db4f9c4e408fb24c2fe7d05a55d5278f8dfd40
epignatelli <edu.pignatelli@gmail.com>
2020-03-22 15:02:09 +0000

    working model

--- model.py
+++ model.py
@@ -1,68 +1,101 @@
-from jax import random, pmap
+import jax
 import jax.numpy as np
-
-class FentonKarma(object):
-    def __init__(self):
-        # Geometry
-        self.height = 128  # units
-        self.width = 128  # units
-        self.dx = dy = 0.025  # mm 
-        self.d2 = dx ** 2
-
-        # state
-        self.voltage = np.zeros((self.height, self.width))
-        self.fast_current = np.ones(self.height, self.width)
-        self.slow_current = np.ones(self.height, self.width)
-        self.time = 0
-
-        # parameters
-        self.cm = 1  # capacitance
-        self.v_c = 0.13
-        self.v_v = 0.04
-        self.tau_d = 0.395
-        self.tau_v1_minus = 9
-        self.tau_v2_minus = 8
-        self.tau_v_plus = 3.33
-        self.tau_0 = 9
-        self.tau_r = 33.33
-        self.tau_si = 29
-        self.v_csi = 0.5
-        self.k = 15
-        self.tau_w_minus = 60
-        self.tau_w_plus = 250
-        self.Dx = 0.001
-        self.Dy = 0.001
-
-        # integration
-        self.dt = 0.1
-
-        # stimulus
-        self.stimulus = np.zeros((height, width))
-        self.stimulus[:40, :40] = -0.2
-
-    def step(self):
+import matplotlib.pyplot as plt
 
 
-
-    def apply_stimulus(self):
-        if 
-
-    def gradient(self, field):
-        
-
-
-
-class Stimulus(object):
-    def __init__(self, value=-0.2, shape=(128, 128)):
-        self.modulus = value
-        self.shape = shape
-        self.protocol = Procotol()
-
-class Protocol(object):
-    def __init__(self, protocol="single"):
-        self.protocol = protocol
-        self.start = 0
-        self.end = -1
-        self.step = -1
+def init(width, height):
+    shape = (width, height)
+    v = np.ones(shape) * 0.99
+    w = np.ones(shape) * 0.99
+    u = np.zeros(shape)
+    state = (v, w, u)   
+    return state
 
 
+@jax.jit
+def step(state, t, params, D, stimuli, dt):
+    v, w, u = state
+    
+    # apply stimulus
+    u = stimulate(t, u, stimuli)
+
+    # apply boundary conditions
+    v = neumann(v)
+    w = neumann(w)
+    u = neumann(u)
+
+    # gate variables
+    p = np.greater_equal(u, params["V_c"])
+    q = np.greater_equal(u, params["V_v"])
+    tau_v_minus = (1 - q) * params["tau_v1_minus"] + q * params["tau_v2_minus"]
+    
+    d_v = ((1 - p) * (1 - v) / tau_v_minus) - ((p * v) / params["tau_v_plus"])
+    d_w = ((1 - p) * (1 - w) / params["tau_w_minus"]) - ((p * w) / params["tau_w_plus"])
+      
+    # currents
+    J_fi = - v * p * (u - params["V_c"]) * (1 - u) / params["tau_d"]
+    J_so = (u * (1 - p) / params["tau_0"]) + (p / params["tau_r"])
+    J_si = - (w * (1 + np.tanh(params["k"] * (u - params["V_csi"])))) / (2 * params["tau_si"])
+    I_ion = -(J_fi + J_so + J_si) / params["Cm"]
+
+    # voltage
+    u_x, u_y = np.gradient(u)
+    u_xx = np.gradient(u_x, axis=0)
+    u_yy = np.gradient(u_y, axis=1)
+    D_x, D_y = np.gradient(D)
+    d_u = 4 * D * (u_xx + u_yy) + ((D_x * u_x) + (D_y * u_y)) + I_ion
+    return euler((v, w, u), (d_v, d_w, d_u), dt)
+
+
+@jax.jit
+def euler(state, grad, dt):
+    v, w, u = state
+    d_v, d_w, d_u = grad
+
+    # explicit euler update
+    v += d_v * dt
+    w += d_w * dt
+    u += d_u * dt
+    return (v, w, u)
+
+
+@jax.jit
+def neumann(X):
+    X = jax.ops.index_update(X, [0], X[1])
+    X = jax.ops.index_update(X, [-1], X[-2])
+    X = jax.ops.index_update(X, [..., 0], X[..., 1])
+    X = jax.ops.index_update(X, [..., -1], X[..., -2])
+    return X
+
+
+@jax.jit
+def stimulate(t, X, stimuli):
+    for stimulus in stimuli:
+        active = t > stimulus["start"]
+        active &= t < stimulus["start"] + stimulus["duration"]
+        # active &= (np.mod(t - stimulus["start"], stimulus["period"]) < stimulus["duration"])  # cyclic
+        X = np.where(stimulus["field"] * (active), stimulus["field"], X)
+    return X
+
+
+@jax.jit
+def forward(length, params, D, stimuli, dt, log_at=10):
+    # iterate
+    state = init(128, 128)
+    state = jax.lax.fori_loop(0, length, lambda i, state: step(state, i * dt, params, D, stimuli, dt), state)
+    return state
+
+
+def show(state, **kwargs):
+    fig, ax = plt.subplots(1, 3, figsize=(kwargs.pop("figsize", None) or (10, 3)))
+    im = ax[0].imshow(state[0], **kwargs)
+    plt.colorbar(im, ax=ax[0])
+    ax[0].set_title("v")
+    im = ax[1].imshow(state[1], **kwargs)
+    plt.colorbar(im, ax=ax[1])
+    ax[1].set_title("w")
+    im = ax[2].imshow(state[2], **kwargs)
+    plt.colorbar(im, ax=ax[2])
+    ax[2].set_title("u")
+    plt.show()
+    return
---
+++ params.py
@@ -0,0 +1,255 @@
+_maxfloat = 1e6
+
+def _params1():
+    params = {
+        "tau_v_plus": 3.33,  # Fast_inward_current_v_gate (ms)
+        "tau_v1_minus": 19.6,  # Fast_inward_current_v_gate (ms)
+        "tau_v2_minus": 1000,  # Fast_inward_current_v_gate (ms)
+        "tau_w_plus": 667,  # Slow_inward_current_w_gate (ms)
+        "tau_w_minus": 11,  # Slow_inward_current_w_gate (ms)
+        "tau_d": NotImplemented,  # excitability
+        "tau_0": 8.3,  # Slow_outward_current (ms)
+        "tau_r": 50,  # Slow_outward_current (ms)
+        "tau_si": 45,  # Slow_inward_current (ms)
+        "k": 10,  # Slow_inward_current (dimensionless)
+        "V_csi": 0.85,  # Slow_inward_current (dimensionless)
+        "V_c": 0.13,
+        "V_v": 0.0055,
+        "Cm": 1,  # membrane capacitance (microF / cm^2)
+    }
+    return params
+
+
+def params1a():
+    params = _params1()
+    params["tau_d"] = 0.41
+    return params
+    
+
+def params1b():
+    params = _params1()
+    params["tau_d"] = 0.392 
+    return params
+    
+
+def params1c():
+    params = _params1()
+    params["tau_d"] = 0.381
+    return params
+    
+
+def params1d():
+    params = _params1()
+    params["tau_d"] = 0.36
+    return params
+    
+
+def params1e():
+    params = _params1()
+    params["tau_d"] = 0.25
+    return params
+
+
+def params2():
+    return {
+        "tau_v_plus": 10,  # Fast_inward_current_v_gate (ms)
+        "tau_v1_minus": 10,  # Fast_inward_current_v_gate (ms)
+        "tau_v2_minus": 10,  # Fast_inward_current_v_gate (ms)
+        "tau_w_plus": _maxfloat,  # Slow_inward_current_w_gate (ms)
+        "tau_w_minus": _maxfloat,  # Slow_inward_current_w_gate (ms)
+        "tau_d": 0.25,  # excitability
+        "tau_0": 10,  # Slow_outward_current (ms)
+        "tau_r": 190,  # Slow_outward_current (ms)
+        "tau_si": _maxfloat,  # Slow_inward_current (ms)
+        "k": 100000,  # Slow_inward_current (dimensionless)
+        "V_csi": _maxfloat,  # Slow_inward_current (dimensionless)
+        "V_c": 0.13,
+        "V_v": _maxfloat,
+        "Cm": 1,  # membrane capacitance (microF / cm^2)
+    }
+
+
+def params3():
+    return {
+        "tau_v_plus": 3.33,  # Fast_inward_current_v_gate (ms)
+        "tau_v1_minus": 19.6,  # Fast_inward_current_v_gate (ms)
+        "tau_v2_minus": 1250,  # Fast_inward_current_v_gate (ms)
+        "tau_w_plus": 870,  # Slow_inward_current_w_gate (ms)
+        "tau_w_minus": 41,  # Slow_inward_current_w_gate (ms)
+        "tau_d": 0.25,  # excitability
+        "tau_0": 12.5,  # Slow_outward_current (ms)
+        "tau_r": 33.33,  # Slow_outward_current (ms)
+        "tau_si": 29,  # Slow_inward_current (ms)
+        "k": 10,  # Slow_inward_current (dimensionless)
+        "V_csi": 0.85,  # Slow_inward_current (dimensionless)
+        "V_c": 0.13,
+        "V_v": 0.04,
+        "Cm": 1,  # membrane capacitance (microF / cm^2)
+    }
+
+
+def _params4():
+    return {
+        "tau_v_plus": 3.33,  # Fast_inward_current_v_gate (ms)
+        "tau_v1_minus": 15.6,  # Fast_inward_current_v_gate (ms)
+        "tau_v2_minus": 5,  # Fast_inward_current_v_gate (ms)
+        "tau_w_plus": 350,  # Slow_inward_current_w_gate (ms)
+        "tau_w_minus": 80,  # Slow_inward_current_w_gate (ms)
+        "tau_d": NotImplemented,  # excitability
+        "tau_0": 9,  # Slow_outward_current (ms)
+        "tau_r": 34,  # Slow_outward_current (ms)
+        "tau_si": 26.5,  # Slow_inward_current (ms)
+        "k": 15,  # Slow_inward_current (dimensionless)
+        "V_csi": 0.45,  # Slow_inward_current (dimensionless)
+        "V_c": 0.15,
+        "V_v": 0.04,
+        "Cm": 1,  # membrane capacitance (microF / cm^2)
+    }
+
+def params4a():
+    params = _params4()
+    params["tau_d"] = 0.407
+    return params
+
+def params4b():
+    params = _params4()
+    params["tau_d"] = 0.405
+    return params
+
+def params4c():
+    params = _params4()
+    params["tau_d"] = 0.4
+    return params
+
+def params5():
+    return {
+        "tau_v_plus": 3.33,  # Fast_inward_current_v_gate (ms)
+        "tau_v1_minus": 12,  # Fast_inward_current_v_gate (ms)
+        "tau_v2_minus": 2,  # Fast_inward_current_v_gate (ms)
+        "tau_w_plus": 1000,  # Slow_inward_current_w_gate (ms)
+        "tau_w_minus": 100,  # Slow_inward_current_w_gate (ms)
+        "tau_d": 0.362,  # excitability
+        "tau_0": 5,  # Slow_outward_current (ms)
+        "tau_r": 33.33,  # Slow_outward_current (ms)
+        "tau_si": 29,  # Slow_inward_current (ms)
+        "k": 15,  # Slow_inward_current (dimensionless)
+        "V_csi": 0.7,  # Slow_inward_current (dimensionless)
+        "V_c": 0.13,
+        "V_v": 0.04,
+        "Cm": 1,  # membrane capacitance (microF / cm^2)
+    }
+
+
+def params6():
+    return {
+        "tau_v_plus": 3.33,  # Fast_inward_current_v_gate (ms)
+        "tau_v1_minus": 9,  # Fast_inward_current_v_gate (ms)
+        "tau_v2_minus": 8,  # Fast_inward_current_v_gate (ms)
+        "tau_w_plus": 250,  # Slow_inward_current_w_gate (ms)
+        "tau_w_minus": 60,  # Slow_inward_current_w_gate (ms)
+        "tau_d": 0.395,  # excitability
+        "tau_0": 9,  # Slow_outward_current (ms)
+        "tau_r": 33.33,  # Slow_outward_current (ms)
+        "tau_si": 29,  # Slow_inward_current (ms)
+        "k": 15,  # Slow_inward_current (dimensionless)
+        "V_csi": 0.5,  # Slow_inward_current (dimensionless)
+        "V_c": 0.13,
+        "V_v": 0.04,
+        "Cm": 1,  # membrane capacitance (microF / cm^2)
+    }
+
+
+def params7():
+    return {
+        "tau_v_plus": 10,  # Fast_inward_current_v_gate (ms)
+        "tau_v1_minus": 7,  # Fast_inward_current_v_gate (ms)
+        "tau_v2_minus": 7,  # Fast_inward_current_v_gate (ms)
+        "tau_w_plus": _maxfloat,  # Slow_inward_current_w_gate (ms)
+        "tau_w_minus": _maxfloat,  # Slow_inward_current_w_gate (ms)
+        "tau_d": 0.25,  # excitability
+        "tau_0": 12,  # Slow_outward_current (ms)
+        "tau_r": 100,  # Slow_outward_current (ms)
+        "tau_si": _maxfloat,  # Slow_inward_current (ms)
+        "k": _maxfloat,  # Slow_inward_current (dimensionless)
+        "V_csi": _maxfloat,  # Slow_inward_current (dimensionless)
+        "V_c": 0.13,
+        "V_v": _maxfloat,
+        "Cm": 1,  # membrane capacitance (microF / cm^2)
+    }
+
+
+def params8():
+    return {
+        "tau_v_plus": 13.03,  # Fast_inward_current_v_gate (ms)
+        "tau_v1_minus": 19.6,  # Fast_inward_current_v_gate (ms)
+        "tau_v2_minus": 1250,  # Fast_inward_current_v_gate (ms)
+        "tau_w_plus": 800,  # Slow_inward_current_w_gate (ms)
+        "tau_w_minus": 40,  # Slow_inward_current_w_gate (ms)
+        "tau_d": 0.45,  # excitability
+        "tau_0": 12.5,  # Slow_outward_current (ms)
+        "tau_r": 33.25,  # Slow_outward_current (ms)
+        "tau_si": 29,  # Slow_inward_current (ms)
+        "k": 10,  # Slow_inward_current (dimensionless)
+        "V_csi": 0.85,  # Slow_inward_current (dimensionless)
+        "V_c": 0.13,
+        "V_v": 0.04,
+        "Cm": 1,  # membrane capacitance (microF / cm^2)
+    }
+
+
+def params9():
+    return {
+        "tau_v_plus": 3.33,  # Fast_inward_current_v_gate (ms)
+        "tau_v1_minus": 15,  # Fast_inward_current_v_gate (ms)
+        "tau_v2_minus": 2,  # Fast_inward_current_v_gate (ms)
+        "tau_w_plus": 670,  # Slow_inward_current_w_gate (ms)
+        "tau_w_minus": 61,  # Slow_inward_current_w_gate (ms)
+        "tau_d": 0.25,  # excitability
+        "tau_0": 12.5,  # Slow_outward_current (ms)
+        "tau_r": 28,  # Slow_outward_current (ms)
+        "tau_si": 29,  # Slow_inward_current (ms)
+        "k": 10,  # Slow_inward_current (dimensionless)
+        "V_csi": 0.45,  # Slow_inward_current (dimensionless)
+        "V_c": 0.13,
+        "V_v": 0.05,
+        "Cm": 1,  # membrane capacitance (microF / cm^2)
+    }
+
+
+def params10():
+    return {
+        "tau_v_plus": 10,  # Fast_inward_current_v_gate (ms)
+        "tau_v1_minus": 40,  # Fast_inward_current_v_gate (ms)
+        "tau_v2_minus": 333,  # Fast_inward_current_v_gate (ms)
+        "tau_w_plus": 1000,  # Slow_inward_current_w_gate (ms)
+        "tau_w_minus": 65,  # Slow_inward_current_w_gate (ms)
+        "tau_d": 0.115,  # excitability
+        "tau_0": 12.5,  # Slow_outward_current (ms)
+        "tau_r": 25,  # Slow_outward_current (ms)
+        "tau_si": 22.22,  # Slow_inward_current (ms)
+        "k": 10,  # Slow_inward_current (dimensionless)
+        "V_csi": 0.85,  # Slow_inward_current (dimensionless)
+        "V_c": 0.13,
+        "V_v": 0.0025,
+        "Cm": 1,  # membrane capacitance (microF / cm^2)
+    }
+
+
+def params_test():
+    return {
+        "Cm": 1,  # membrane capacitance (microF / cm^2)
+        "V_c": 0.16,
+        "V_v": 0.16,
+        "tau_d": 0.125, # excitability
+        "tau_v1_minus": 82.5,  # Fast_inward_current_v_gate (ms)
+        "tau_v2_minus": 60,  # Fast_inward_current_v_gate (ms)
+        "tau_v_plus": 5.75,  # Fast_inward_current_v_gate (ms)
+        "tau_0": 32.5,  # Slow_outward_current (ms)
+        "tau_r": 70,  # Slow_outward_current (ms)
+        "tau_si": 114,  # Slow_inward_current (ms)
+        "V_csi": 0.85,  # Slow_inward_current (dimensionless)
+        "k": 10,  # Slow_inward_current (dimensionless)
+        "tau_w_minus": 400,  # Slow_inward_current_w_gate (ms)
+        "tau_w_plus": 300,  # Slow_inward_current_w_gate (ms)
+        "D": 0.05,  # diffusivity
+}
