579d4c97473f8b2b60c1fa50a23923a4279162ab
Eduardo Pignatelli <ep119@ic.ac.uk>
2020-07-15 13:31:36 +0100

    working 3d unet

--- deepexcite/dataset.py
+++ deepexcite/dataset.py
@@ -3,62 +3,33 @@ import numpy as np
 import h5py
 import torch
 import fk
+import random
 
 
 class FkDataset():
     def __init__(self, root, n_frames_in=5, n_frames_out=10, step=1,
-                 keys=None, transforms=None, squeeze=False):
+                 keys=None, transform=None, squeeze=False):
         self.root = root
         self.n_frames_in = n_frames_in
         self.n_frames_out = n_frames_out
         self.n_frames = n_frames_in + n_frames_out
         self.step = step
-        self.transforms = transforms
         self.squeeze = squeeze
         
         filenames = [os.path.join(root, name) for name in sorted(os.listdir(root)) if name.endswith("hdf5")]
         if keys is not None:
             filenames = [name for name in filenames 
                  if os.path.basename(name) in keys ]
-        self.datasets = [Simulation(filename, n_frames_in, n_frames_out) for filename in filenames]
-        self.cumulative_sizes = self.cumsum(self.datasets)
+        self.datasets = [Simulation(filename, n_frames_in, n_frames_out, step, transform) for filename in filenames]
         
-    @staticmethod
-    def cumsum(sequence):
-        r, s = [], 0
-        for e in sequence:
-            l = len(e)
-            r.append(l + s)
-            s += l
-        return r
 
     def __len__(self):
-        return self.cumulative_sizes[-1]
+        return len(self.datasets[0])
 
     def __getitem__(self, idx):
-        if idx < 0:
-            if -idx > len(self):
-                raise ValueError("absolute value of index should not exceed dataset length")
-            idx = len(self) + idx
-        dataset_idx = torch.utils.data.dataset.bisect.bisect_right(self.cumulative_sizes, idx)
-        if dataset_idx == 0:
-            sample_idx = idx
-        else:
-            sample_idx = idx - self.cumulative_sizes[dataset_idx - 1]
-        start = sample_idx
-        end = start + self.n_frames_in + self.n_frames_out
-        if end > len(self):
-            return None
-    
-        sample = np.array(self.datasets[dataset_idx][start:end:self.step])#["states"]
-        sample = torch.as_tensor(sample)
-        
-        if self.transforms is not None:
-            sample = self.transforms(sample)
+        dataset = random.choice(self.datasets)
+        sample = dataset[idx]
 
-        if self.squeeze:
-            sample = torch.squeeze(sample)
-            
         return sample
 
     def close(self):
@@ -66,10 +37,11 @@ class FkDataset():
             dataset.states.file.close()
     
 class Simulation():
-    def __init__(self, filename, in_frames=1, out_frames=0, step=1):
+    def __init__(self, filename, in_frames=1, out_frames=0, step=1, transform=None):
         self.in_frames = in_frames
         self.out_frames = out_frames
         self.step = step
+        self.transform = transform
         
         self.filename = filename
         file = h5py.File(filename, "r") 
@@ -83,8 +55,13 @@ class Simulation():
     def __getitem__(self, idx):
         if isinstance(idx, slice):
             idx = slice(idx.start, idx.start + (self.in_frames + self.out_frames) * self.step, self.step)
-            return np.array(self.states[idx])
-        states = np.array(self.states[idx: idx + (self.in_frames + self.out_frames) * self.step: self.step])
+            states = np.array(self.states[idx])
+        else:
+            states = np.array(self.states[idx: idx + (self.in_frames + self.out_frames) * self.step: self.step])
+        
+        if self.transform is not None:
+            states = self.transform(states)
+        
         return states
 #         unstimulated = np.zeros(self.states.shape[-2:])
 #         stimuli = np.stack([self.stimulus_at_t(t) for t in range(idx.start, idx.stop, idx.step)])
---
+++ deepexcite/utils.py
@@ -0,0 +1,47 @@
+import torch
+from torch import nn
+
+
+def log(*m):
+    if DEBUG:
+        print(*m)
+
+def gradient(x):
+    log("grad", x.shape)
+    left = x
+    right = nn.functional.pad(x, [0, 1, 0, 0])[..., :, 1:]
+    top = x
+    bottom = nn.functional.pad(x, [0, 0, 0, 1])[..., 1:, :]
+    dx, dy = right - left, bottom - top
+    dx[..., :, -1] = 0
+    dy[..., -1, :] = 0
+    log(dx.shape, dy.shape)
+    return torch.abs(dx) + torch.abs(dy)        
+        
+    
+def grad_mse_loss(gen_frames, gt_frames, reduction="sum"):
+    grad_pred = gradient(gen_frames)
+    grad_truth = gradient(gt_frames)
+
+    # condense into one tensor and avg
+    return nn.functional.mse_loss(grad_pred, grad_truth, reduction=reduction)
+
+
+class Elu(nn.Module):
+    def forward(self, x):
+        return torch.nn.functional.elu(x)
+    
+    
+class Downsample:
+    def __init__(self, size, mode="bicubic"):
+        self.size = size
+        self.mode = mode
+    
+    def __call__(self, x):
+        return nn.functional.interpolate(x, self.size)
+
+    
+class Normalise:
+    def __call__(self, x):
+        return (x - x.mean()) / x.std()
+    
