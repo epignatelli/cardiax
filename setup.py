3679534d3ef226edf6a415045d38a0554f9f5998
Eduardo <eduardo.pignatelli@burohappold.com>
2021-02-26 12:40:20 +0000

    remove jp from req

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     description="A JAX implementation of various reaction-diffusion models",
     author="Eduardo Pignatelli",
     author_email="edu.pignatelli@gmail.com",
-    url="https://github.com/epignatelli/fenton_karma_jax",
+    url="https://github.com/epignatelli/cardiax",
     packages=["cardiax", "deepx"],
 )
