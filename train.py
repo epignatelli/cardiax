d5d22664ab4aff46bc90e9df35af043470f98dc6
Eduardo <eduardo.pignatelli@burohappold.com>
2021-03-15 08:53:11 +0000

    add separate validation step

--- train.py
+++ train.py
@@ -30,8 +30,9 @@ flags.DEFINE_float("lr", 0.0001, "")
 flags.DEFINE_integer("batch_size", 4, "")
 flags.DEFINE_integer("evaluation_steps", 20, "")
 flags.DEFINE_integer("epochs", 100, "")
-flags.DEFINE_integer("maxsteps", 100, "")
-flags.DEFINE_float("increase_at", 0.0, "")
+flags.DEFINE_integer("train_maxsteps", 100, "")
+flags.DEFINE_integer("val_maxsteps", 10, "")
+flags.DEFINE_float("increase_at", 0.01, "")
 flags.DEFINE_float("teacher_forcing_prob", 0.0, "")
 flags.DEFINE_string("from_checkpoint", "", "")
 
@@ -48,32 +49,21 @@ flags.DEFINE_boolean("preload", False, "")
 FLAGS = flags.FLAGS
 
 
-def train():
-    pass
-
-
-def evaluate():
-    pass
-
-
-def schedule():
-    pass
-
-
 def main(argv):
     logging.basicConfig(
         stream=sys.stdout, level=logging.DEBUG if FLAGS.debug else logging.INFO
     )
-    #  unroll hparams
+    #  hparms
     logging.info("Parsing hyperparamers and initialising logger...")
     hparams = resnet.HParams.from_flags(FLAGS)
     refeed = hparams.refeed
     epochs = hparams.epochs
-    maxsteps = hparams.maxsteps if not hparams.debug else 6
+    train_maxsteps = hparams.train_maxsteps if not hparams.debug else 1
+    val_maxsteps = hparams.val_maxsteps if not hparams.debug else 1
     log_frequency = hparams.log_frequency
     wandb.init(project="deepx")
 
-    # save hparams
+    #  log
     logging.info("Logging hyperparameters...")
     list(
         map(
@@ -83,7 +73,7 @@ def main(argv):
     )
     n_sequence_out = hparams.refeed * hparams.frames_out
 
-    # get data streams
+    #  datasets
     input_shape = (
         hparams.batch_size,
         hparams.frames_in,
@@ -102,28 +92,32 @@ def main(argv):
     train_set = make_dataset("train")
     val_set = make_dataset("val")
 
-    # init model
+    #  init
     logging.info("Initialising model...")
     rng = jax.random.PRNGKey(hparams.seed)
+    rng_val = jax.random.PRNGKey(hparams.seed)
     model = resnet.ResNet(
         hidden_channels=hparams.hidden_channels,
         out_channels=hparams.frames_out,
         depth=hparams.depth,
     )
-
-    # init optimiser
     logging.info("Initialising optimisers...")
     _, params = model.init(rng, input_shape)
+    if hparams.from_checkpoint not in ("", None):
+        with open(hparams.from_checkpoint, "rb") as f:
+            params = pickle.load(f)
     optimiser = Optimiser(*optimizers.adam(hparams.lr))
     optimiser_state = optimiser.init(params)
     with open("{}/{}.pickle".format(wandb.run.dir, "start"), "wb") as f:
         pickle.dump(params, f)
     logging.info("Starting training...")
+    global_step = 0
     for i in range(epochs):
-        ## TRAINING
+        #  train
         train_loss_epoch = 0.0
-        for j in range(maxsteps):
-            k = (maxsteps * i) + j
+        for j in range(train_maxsteps):
+            k = (train_maxsteps * i) + j
+            global_step += 1
             rng, _ = jax.random.split(rng)
             xs, ys = train_set.sample(rng)
             j_train, ys_hat, optimiser_state = optimise.tbtt_step(
@@ -131,29 +125,49 @@ def main(argv):
             )
             train_loss_epoch += j_train
             optimise.log_train(
-                i, epochs, k, maxsteps, j_train, xs, ys_hat, ys, log_frequency
+                i,
+                epochs,
+                k,
+                train_maxsteps,
+                j_train,
+                xs,
+                ys_hat,
+                ys,
+                log_frequency,
+                global_step,
             )
 
-        # ## VALIDATING
-        # params = optimiser.params(optimiser_state)
-        # for j in enumerate(maxsteps):
-        #     k = (maxsteps * i) + j
-        #     rng, _ = jax.random.split(rng)
-        #     xs, ys = train_set.sample(rng)
-        #     j_val, ys_hat = optimise.evaluate(
-        #         model, hparams.evaluation_steps, params, xs, ys
-        #     )
+        #  validate
+        params = optimiser.params(optimiser_state)
+        _rng_val = rng_val
+        for j in range(val_maxsteps):
+            k = (val_maxsteps * i) + j
+            global_step += 1
+            _rng_val, _ = jax.random.split(_rng_val)
+            xs, ys = val_set.sample(_rng_val)
+            j_val, ys_hat = optimise.evaluate(model, refeed, params, xs, ys)
+            optimise.log_val(
+                i,
+                epochs,
+                k,
+                val_maxsteps,
+                j_val,
+                xs,
+                ys_hat,
+                ys,
+                log_frequency,
+                global_step,
+            )
 
-        #     # logging
-        #     optimise.log_val(i, epochs, k, j_val, xs, ys_hat, ys, k)
-
-        ## SCHEDULER
-        train_loss_epoch /= train_set.num_batches()
+        #  schedule
+        train_loss_epoch /= train_maxsteps
         if (i != 0) and (train_loss_epoch <= hparams.increase_at) and (refeed < 20):
+            refeed += 1
             train_set.increase_frames()
-        wandb.log({"refeed": refeed}, k)
+            val_set.increase_frames()
+        wandb.log({"refeed": refeed, "epoch": i}, step=global_step)
 
-        ## Save checkpoint
+        #  checkpoint
         with open("{}/{}.pickle".format(wandb.run.dir, i), "wb") as f:
             pickle.dump(params, f)
 
